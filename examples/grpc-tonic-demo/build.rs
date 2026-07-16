41ea6131200bc430c74e4c0c4897cdf9edef2a80
Sanghyo Lee <ryan@cellaxon.com>
2026-07-16 22:12:55 +0900

    ci: make cross-platform compile self-contained

--- examples/grpc-tonic-demo/build.rs
+++ examples/grpc-tonic-demo/build.rs
@@ -1,4 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/greeter.proto")?;
+    let protoc = protoc_bin_vendored::protoc_bin_path()?;
+    let mut prost = tonic_build::Config::new();
+    prost.protoc_executable(protoc);
+    tonic_build::configure().compile_protos_with_config(
+        prost,
+        &["proto/greeter.proto"],
+        &["proto"],
+    )?;
     Ok(())
 }
